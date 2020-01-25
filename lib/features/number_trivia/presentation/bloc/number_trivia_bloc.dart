d6e1f4214b60294d37d64158a0826f40e80eedca
ivlup <ivlupsecond@gmail.com>
2020-01-25 10:47:35 +0200

    Bloc Implementation 1/2

---
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_number_trivia/core/util/input_converter.dart';
+import 'package:flutter_number_trivia/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:flutter_number_trivia/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+import 'package:flutter_number_trivia/features/number_trivia/presentation/bloc/number_trivia_event.dart';
+import 'package:flutter_number_trivia/features/number_trivia/presentation/bloc/number_trivia_state.dart';
+
+const String SERVER_FAILURE_MESSAGE = 'Server failure';
+const String CACHE_FAILURE_MESSAGE = 'Cahce failure';
+const String INVALID_INPUT_FAILURE_MESSAGE =
+    'Invalid input - The number must be a positive integer or zero';
+
+class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+  final InputConverter inputConverter;
+
+  NumberTriviaBloc({
+    @required GetConcreteNumberTrivia concrete,
+    @required GetRandomNumberTrivia random,
+    @required this.inputConverter,
+  })  : assert(concrete != null),
+        assert(random != null),
+        assert(inputConverter != null),
+        getConcreteNumberTrivia = concrete,
+        getRandomNumberTrivia = random;
+
+  @override
+  NumberTriviaState get initialState => Empty();
+
+  @override
+  Stream<NumberTriviaState> mapEventToState(NumberTriviaEvent event) async* {
+    if (event is GetTriviaForConcreteNumber) {
+      final inputEither =
+          inputConverter.stringToUnsignedInteger(event.numberString);
+
+      yield* inputEither.fold(
+        (failure) async* {
+          yield Error(message: INVALID_INPUT_FAILURE_MESSAGE);
+        },
+        (integer) => throw UnimplementedError(),
+      );
+    }
+  }
+}
--- lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
@@ -1,6 +1,5 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_number_trivia/core/error/failures.dart';
 import 'package:flutter_number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 
 abstract class NumberTriviaState extends Equatable {
